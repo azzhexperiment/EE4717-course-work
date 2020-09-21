78513948d026959b2bfd7f10cbfe2b442d82b96e
ZHU Zihao <zihao@zhuzihao.me>
2020-09-22 02:28:39 +0800

    :sparkles: add validation methods

--- Week 06/CaseStudyPt3/assets/js/validate.js
+++ Week 06/CaseStudyPt3/assets/js/validate.js
@@ -1,51 +1,129 @@
-// Script - validate.js
-
-// Detects that a field is active
-// Standby event
-// Call validation function
-// On submit, run all validation again
-
 /**
- * Function called when a form field loses focus
- * Validates the field, throws error message if non-compliant with rules
+ * Script - validate.js
  *
+ * @author Zhu Zihao <zhuz0010@ntu.edu.sg>
+ * @version 1.0
  */
 
-// Function called when the form is submitted.
-// Function validates data and returns a Boolean value.
-function validateField(selectedField) {
-  'use strict'
+/**
+ * Modify the “Jobs” page so that
+ * the input data for name, email, start date and experience
+ * are validated once the data is entered into each field.
+ *
+ * Modify the “Menu” page to include order quantity
+ * and options for single or double shots.
+ *
+ * The sub-total for each order should be displayed
+ * along the total amount below the sub-totals.
+ * The sub-totals and total should be computed using JavaScript function.
+ */
 
-  var field = selectedField
+const form = document.getElementById('applyJob')
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const date = document.getElementById('date')
 
-  // use a switch statement to run the corresponding validation
-}
-
-function validateEmail(email) {
-
-}
-
-function validateName(name) {
-
-}
-
-function validateDate(date) {
-
-}
-
-// Function called when the window has been loaded.
-// Function needs to add an event listener to the form.
-function init() {
-  'use strict'
-
-  // Confirm that document.getElementById() can be used
-  if (document && document.getElementById) {
-    var applyJob = document.getElementById('applyJob')
-    var activeField = document.activeElement.id
-
-    applyJob.onfocus = validateField(activeField)
+/**
+ * Listens to the <name> field and assess validity
+ */
+name.addEventListener('input', (event) => {
+  if (isValidName(name.value)) {
+    name.setCustomValidity('Invalid format')
+  } else {
+    name.setCustomValidity('')
   }
+})
+
+/**
+ * Listens to the <email> field and assess validity
+ */
+email.addEventListener('input', (event) => {
+  if (isValidName(email.value)) {
+    email.setCustomValidity('Invalid format')
+  } else {
+    email.setCustomValidity('')
+  }
+})
+
+/**
+ * Listens to the <date> field and assess validity
+ */
+date.addEventListener('input', (event) => {
+  if (isValidName(date.value)) {
+    date.setCustomValidity('Invalid format')
+  } else {
+    date.setCustomValidity('')
+  }
+})
+
+/**
+ * Validate everything before submission just in case prof asks
+ */
+form.addEventListener('submit', (event) => {
+  const test = email.value.length === 0 || emailRegExp.test(email.value);
+
+  if (!test) {
+    email.className = "invalid";
+    error.innerHTML = "I expect an e-mail, darling!";
+    error.className = "error active";
+
+    // Some legacy browsers do not support the event.preventDefault() method
+    return false;
+  } else {
+    email.className = "valid";
+    error.innerHTML = "";
+    error.className = "error";
+  }
+});
+
+/**
+ * The email field contains a user name part
+ * follows by “@” and a domain name part.
+ * The user name contains word characters
+ * including hyphen (“-”) and period (“.”).
+ *
+ * The domain name contains two to four address extensions.
+ * Each extension is string of word characters and
+ * separated from the others by a period (“.”).
+ * The last extension must have two to three characters.
+ *
+ * @param {String} value
+ * @returns {Boolean}
+ */
+function isValidEmail (value) {
+  var rule = /^[\w.-]+@([\w-]+\.){1,3}[a-zA-Z]{2,3}$/
+
+  value.trim()
+
+  value.test(rule) ? true : false
+
 }
 
-// Assign an event listener to the window's load event
-window.onload = init
+/**
+ * The name field contains alphabet characters and character spaces.
+ *
+ * @param {String} value
+ * @returns {Boolean}
+ */
+function isValidName (value) {
+  var rule = /[a-zA-Z ]/
+
+  value.trim()
+
+  value.test(rule) ? true : false
+}
+
+/**
+ * The start date cannot be from today and the past.
+ *
+ * @param {Date} value
+ * @returns {Boolean}
+ */
+function isValidDate (value) {
+  var rule = /[a-zA-Z ]/
+
+  value.trim()
+
+  value.test(rule) ? true : false
+
+}

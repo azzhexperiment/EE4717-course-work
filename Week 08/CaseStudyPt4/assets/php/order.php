f20b04c8556f28555779a68d1d07513f25cedde1
ZHU Zihao <zihao@zhuzihao.me>
2020-09-29 00:24:31 +0800

    :children_crossing: Delegate alert to js

--- Week 08/CaseStudyPt4/assets/php/order.php
+++ Week 08/CaseStudyPt4/assets/php/order.php
@@ -21,22 +21,6 @@ $iced_cappuccino_subtotal = $iced_cappuccino_qty * $iced_cappuccino_price;
 
 $total = $just_java_subtotal + $cafe_au_lait_subtotal + $iced_cappuccino_subtotal;
 
-// echo '<pre>', var_dump($_POST), '</pre>';
-// echo '<pre>', var_dump($total), '</pre>';
-
-// echo 'Just Java (qty, price, subtotal)';
-// echo '<pre>', var_dump($just_java_qty),            '</pre>';
-// echo '<pre>', var_dump($just_java_price),          '</pre>';
-// echo '<pre>', var_dump($just_java_subtotal),       '</pre>';
-// echo 'Cafe au Lait (qty, price, subtotal)';
-// echo '<pre>', var_dump($cafe_au_lait_qty),         '</pre>';
-// echo '<pre>', var_dump($cafe_au_lait_price),       '</pre>';
-// echo '<pre>', var_dump($cafe_au_lait_subtotal),    '</pre>';
-// echo 'Iced Cappuccino (qty, price, subtotal)';
-// echo '<pre>', var_dump($iced_cappuccino_qty),      '</pre>';
-// echo '<pre>', var_dump($iced_cappuccino_price),    '</pre>';
-// echo '<pre>', var_dump($iced_cappuccino_subtotal), '</pre>';
-
 $db = new mysqli('localhost', 'f37ee', 'f37ee', 'f37ee');
 
 // Check connection
@@ -64,9 +48,4 @@ $db->query($query);
 
 $db->close();
 
-echo '
-	<script>
-		alert("Order placed!")
-		window.close()
-	</script>
-';
+echo '<script>window.close()</script>';
